991c717ad24412cacfe6d4ee3e74c21d5dbbbe58
Intezar Alam <tanjavoorathefe@gmail.com>
2024-07-31 13:25:42 +0530

    re-build the youtube application using next js ,typescript,tailwind css

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,6 @@
+import { cn } from "@/lib/utils/tailwind-utils";
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
