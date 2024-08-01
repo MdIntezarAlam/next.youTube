23b62fcdd08ac23d319d835d7fd637ca267e6e9b
Intezar Alam <tanjavoorathefe@gmail.com>
2024-08-01 20:34:20 +0530

    updated

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -14,12 +14,62 @@ export const Sidebar = ({
 }: SidebarProps) => {
   return (
     <div className="flex lg:flex-col gap-3 py-1 p-4">
-      {uniqueCategories.map((item: ICategory, i) => (
+      <h2 className="bg-secondary h-0.5 w-full my-2" />
+      <h1 className="text-lg font-medium">Favourites</h1>
+      {uniqueCategories?.slice(0, 5).map((item: ICategory, i) => (
         <button
           key={i}
           onClick={() => setSelectedCategory(item?.name)}
           className={cn(
-            "flex items-center gap-2 lg:gap-4 rounded-full lg:rounded-xl px-5 py-1 lg:p-3 text-sm lg:text-xl opacity-95",
+            "flex items-center gap-2 lg:gap-4 rounded-full lg:rounded-xl  px-5 py-1 lg:p-3 text-sm lg:text-xl opacity-95 ",
+            selectedCategory === item?.name &&
+              "bg-card opacity-100 text-destructive"
+          )}
+        >
+          {item.icons}
+          <h1 className="text-sm font-normal">{item?.name}</h1>
+        </button>
+      ))}
+      <h2 className="bg-secondary h-0.5 w-full my-2" />
+      <h1 className="text-lg font-medium">Most Popular</h1>
+      {uniqueCategories?.slice(5, 10).map((item: ICategory, i) => (
+        <button
+          key={i}
+          onClick={() => setSelectedCategory(item?.name)}
+          className={cn(
+            "flex items-center gap-2 lg:gap-4 rounded-full lg:rounded-xl  px-5 py-1 lg:p-3 text-sm lg:text-xl opacity-95 ",
+            selectedCategory === item?.name &&
+              "bg-card opacity-100 text-destructive"
+          )}
+        >
+          {item.icons}
+          <h1 className="text-sm font-normal">{item?.name}</h1>
+        </button>
+      ))}
+      <h2 className="bg-secondary h-0.5 w-full my-2" />
+      <h1 className="text-lg font-medium">Explore</h1>
+      {uniqueCategories?.slice(10,15).map((item: ICategory, i) => (
+        <button
+          key={i}
+          onClick={() => setSelectedCategory(item?.name)}
+          className={cn(
+            "flex items-center gap-2 lg:gap-4 rounded-full lg:rounded-xl  px-5 py-1 lg:p-3 text-sm lg:text-xl opacity-95 ",
+            selectedCategory === item?.name &&
+              "bg-card opacity-100 text-destructive"
+          )}
+        >
+          {item.icons}
+          <h1 className="text-sm font-normal">{item?.name}</h1>
+        </button>
+      ))}
+      <h2 className="bg-secondary h-0.5 w-full my-2" />
+      <h1 className="text-lg font-medium">More From YouTube</h1>
+      {uniqueCategories?.slice(15).map((item: ICategory, i) => (
+        <button
+          key={i}
+          onClick={() => setSelectedCategory(item?.name)}
+          className={cn(
+            "flex items-center gap-2 lg:gap-4 rounded-full lg:rounded-xl  px-5 py-1 lg:p-3 text-sm lg:text-xl opacity-95 ",
             selectedCategory === item?.name &&
               "bg-card opacity-100 text-destructive"
           )}
